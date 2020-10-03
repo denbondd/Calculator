b12f2ba07ecbc16c16fbdbd1e546102c587a5252
Denis Bondarenko <denbon04@gmail.com>
2020-10-03 20:57:37 +0300

    add basics operations and cleanAll

--- docs/script.js
+++ docs/script.js
@@ -4,9 +4,49 @@ let webElements = {
     secondInput: document.querySelector(".secondInput"),
 };
 
-webElements.buttons.forEach(function(button) {
-    button.addEventListener('click', function(e) {
-        webElements.secondInput.innerHTML += e.target.innerHTML;
-        webElements.firstInput.innerHTML += e.target.innerHTML;
+webElements.buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        text = e.target.innerHTML;
+        switch (text) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                webElements.firstInput.innerHTML = ` ${webElements.secondInput.innerHTML} ${text}`
+                webElements.secondInput.innerHTML = '';
+                break;
+            case '=':
+                equalsBtn(webElements.firstInput.innerHTML, webElements.secondInput.innerHTML);
+                break;
+            case 'C':
+                webElements.secondInput.innerHTML = '';
+                webElements.firstInput.innerHTML = '';
+                break;
+            default:
+                webElements.secondInput.innerHTML += e.target.innerHTML;
+                break;
+        }
     })
 });
+
+function equalsBtn(firstNumberAndSign, secondNumber) {
+    //get number from a whole string
+    let firstNumber = +firstNumberAndSign.split('').filter(a => isFinite(a) || a === '.').join('');
+    webElements.firstInput.innerHTML += ` ${webElements.secondInput.innerHTML} =`;
+    let answ;
+    if (firstNumberAndSign.includes('+')) {
+        answ = firstNumber + +secondNumber;
+    } else if (firstNumberAndSign.includes('-')) {
+        answ = firstNumber - secondNumber;
+    } else if (firstNumberAndSign.includes('*')) {
+        answ = firstNumber * secondNumber;
+    } else if (firstNumberAndSign.includes('/')) {
+        answ = firstNumber / secondNumber;
+    } else {
+        answ = firstNumber + +secondNumber;
+    }
+    if (+answ % 0.000001 != 0) {
+        answ = (Math.round(answ * 100_000_000)) / 100_000_000;
+    }
+    webElements.secondInput.innerHTML = answ;
+}
